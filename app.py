e84aa013b13b8de74c3f1ba11308352fa9c2ab2b
Pavankumar Hegde <puhegde01@gmail.com>
2023-03-21 21:10:49 +0530

    commit

---
+++ app.py
@@ -0,0 +1,13 @@
+import os
+from random import randint
+
+for i in range(1, 1365):
+    
+    for j in range(0, randint(1, 100)):
+        d = str(i) + 'days ago'
+        with open('file.txt' , 'a') as file:
+            file.write(d)
+            os.system('git add .')
+            os.system('git commit --date="' + d + '" -m "commit"')
+            
+os.system('git push  -u origin master')
